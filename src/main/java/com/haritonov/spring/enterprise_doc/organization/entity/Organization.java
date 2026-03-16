39c23cd90126897799b4a94c4bc24519fd9cc7a9
Nikita Haritonov <nikita.harit@mail.ru>
2026-03-16 18:25:01 +0300

    feat(organization): implement Organization entity linked to Account

---
+++ src/main/java/com/haritonov/spring/enterprise_doc/organization/entity/Organization.java
@@ -0,0 +1,39 @@
+package com.haritonov.spring.enterprise_doc.organization.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "organization")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Organization {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    @Column(name = "address")
+    @NotNull
+    private String address;
+
+    @Column(name = "chief")
+    @NotNull
+    private String chief;
+
+    @Column(name = "financial_chief")
+    @NotNull
+    private String financialChief;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
+    @NotNull
+    private Account account;
+}
