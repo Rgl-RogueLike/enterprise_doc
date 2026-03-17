4f6f4688361feeddffe3b1ff499409851e7910c7
Nikita Haritonov <nikita.harit@mail.ru>
2026-03-17 12:35:32 +0300

    fix(organization): correct generic type in AccountRepository from AccountRepository to Account

--- src/main/java/com/haritonov/spring/enterprise_doc/organization/repository/AccountRepository.java
+++ src/main/java/com/haritonov/spring/enterprise_doc/organization/repository/AccountRepository.java
@@ -1,9 +1,10 @@
 package com.haritonov.spring.enterprise_doc.organization.repository;
 
+import com.haritonov.spring.enterprise_doc.organization.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends JpaRepository<AccountRepository, Integer> {
+public interface AccountRepository extends JpaRepository<Account, Integer> {
 
 }
