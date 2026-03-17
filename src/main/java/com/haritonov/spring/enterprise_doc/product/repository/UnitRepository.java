db87d1f567e919ec50a6bf008a74c9aa242b1fc3
Nikita Haritonov <nikita.harit@mail.ru>
2026-03-17 11:15:52 +0300

    refactor(product): remove unused List import from UnitRepository

--- src/main/java/com/haritonov/spring/enterprise_doc/product/repository/UnitRepository.java
+++ src/main/java/com/haritonov/spring/enterprise_doc/product/repository/UnitRepository.java
@@ -4,8 +4,6 @@ import com.haritonov.spring.enterprise_doc.product.entity.Unit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UnitRepository extends JpaRepository<Unit, Integer> {
 
